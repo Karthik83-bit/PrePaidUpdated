9ca08a60b9eb328b4ca8997260e05497e47b64d3
sunidhi.chattar <sunidhi.chattar@iserveu.co.in>
2023-08-19 16:33:02 +0530

    Card Data By Customer

---
+++ PrepaidCardSDK/src/main/java/com/example/prepaidcardsdk/domain/usecases/CardDataUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.prepaidcardsdk.domain.usecases
+
+import com.example.prepaidcardsdk.data.model.req.CardDataRequestModel
+import com.example.prepaidcardsdk.data.model.resp.CardDataResponse
+import com.example.prepaidcardsdk.domain.repo.Repository
+import com.example.prepaidcardsdk.utils.NetworkResponse
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CardDataUseCase @Inject constructor(
+    private val repository: Repository
+){
+    fun invoke(url:String, requestModel: CardDataRequestModel): Flow<NetworkResponse<CardDataResponse>>{
+        return repository.cardDataStatus(url, requestModel)
+    }
+}
